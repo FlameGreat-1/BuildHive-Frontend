019e39176243fbfa6cfa2c53bfdb67e0ef42c78e
Tennin <divinejovakporaye@gmail.com>
2025-07-08 10:49:23 +0100

    changed

--- src/pages/landingPage/index.tsx
+++ src/pages/landingPage/index.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 const LandingPage: React.FC = () => {
     return (
-        <div className='w-screen overflow-x-none overflow-y-auto h-[100dvh] bg-light-white text-purple-bg'>
+        <div className='w-screen overflow-x-none overflow-y-auto  bg-light-white text-purple-bg'>
             <header className=' bg-light-white  flex-center w-full z-10'>
                 <NavBar />
             </header>
