0520370be77018fb96d8c5c5c6e40d252165e382
Tennin <divinejovakporaye@gmail.com>
2025-08-08 06:52:29 +0100

    fixed bugs

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  optimizeDeps:{
+    include:['react-router-dom']
+  },
   plugins: [react()],
   resolve: {
     alias: {
       '@': '/src', // This allows you to use '@' as an alias for the 'src' directory,
-    }   
+    },
+    dedupe:['react-router-dom']
   }
 })
