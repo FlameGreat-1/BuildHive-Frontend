da7d89985cf58c9f6862d145f738556ff97dcc2a
Tennin <divinejovakporaye@gmail.com>
2025-09-02 11:32:50 +0100

    made changes

--- src/userRoles/client/browseTradies/components/TradieComponent.tsx
+++ src/userRoles/client/browseTradies/components/TradieComponent.tsx
@@ -13,34 +13,33 @@ export interface TradieCompProps {
 
 const TradieComponent = ({ pic, name, address, rating, noOfRatings, occupation }: TradieCompProps) => {
     return (
-        <Link to={`${name.split(' ').join(('-'))}`}>
-
-            <div className='flex w-full min-w-[300px]  border border-primary-purple rounded-md shadow-md justify-between p-2 '>
-                <div className="flex gap-2 w-fit">
-                    <img
-                        className='w-12 aspect-square object-contain md:w-16'
-                        src={pic}
-                        alt="Profile Pic" />
-                    <div className="flex flex-col overflow-hidden">
-                        <p className="text-black font-semibold capitalize">{name}</p>
-                        <address
-                            title={address}
-                            className="text-gray-500 max-w-[120px] sm:max-w-[150px] md:max-w-[160px] text-sm truncate  ">{address}</address>
-                        <Rating
-                            size='small'
-                            name='read-only-rating'
-                            value={rating ?? 0}
-                            precision={0.5}
-                            readOnly />
-                        <p className="text-xs md:text-sm">{noOfRatings} Reviews</p>
-                    </div>
-                </div>
-                <div className="flex flex-col gap-2 items-end w-fit">
-                    <p className="text-black capitalize">{occupation}</p>
-                    <p className='border border-primary-purple rounded-md w-fit p-1 px-2 hover:cursor-pointer hover:font-bold text-center text-sm text-nowrap '>View Profile</p>
+        <div className='flex w-full min-w-[300px]  border border-primary-purple rounded-md shadow-md justify-between p-2 '>
+            <div className="flex gap-2 w-fit">
+                <img
+                    className='w-12 aspect-square object-contain md:w-16'
+                    src={pic}
+                    alt="Profile Pic" />
+                <div className="flex flex-col overflow-hidden">
+                    <p className="text-black font-semibold capitalize">{name}</p>
+                    <address
+                        title={address}
+                        className="text-gray-500 max-w-[120px] sm:max-w-[150px] md:max-w-[160px] text-sm truncate  ">{address}</address>
+                    <Rating
+                        size='small'
+                        name='read-only-rating'
+                        value={rating ?? 0}
+                        precision={0.5}
+                        readOnly />
+                    <p className="text-xs md:text-sm">{noOfRatings} Reviews</p>
                 </div>
             </div>
-        </Link>
+            <div className="flex flex-col gap-2 items-end w-fit">
+                <p className="text-black capitalize">{occupation}</p>
+                <Link to={`${name.split(' ').join(('-'))}`}>
+                    <p className='border border-primary-purple rounded-md w-fit p-1 px-2 hover:cursor-pointer hover:text-white hover:bg-accent-purple hover:font-bold text-center text-sm text-nowrap '>View Profile</p>
+                </Link>
+            </div>
+        </div>
     )
 }
 
